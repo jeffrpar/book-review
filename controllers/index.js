30fe1781b94133947cd09035bda3623e0af01645
puze zhong <zpz9991@gmail.com>
2023-08-06 15:03:25 -0500

    update

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
 
 const homeControllers = require('./homeControllers.js');
-const exchangeControllers = require('./exchangeController.js');
 const bookControllers = require('./bookControllers.js');
 const userControllers = require('./userControllers.js');
 
 router.use('/', homeControllers);
-router.use('/api/exchanges', exchangeControllers);
 router.use('/api/books', bookControllers);
 router.use('/api/users', userControllers);
 
