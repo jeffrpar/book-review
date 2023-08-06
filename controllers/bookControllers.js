a7096848de951098586a35b231138f771b25ff67
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-08-05 20:01:42 -0500

    integrated search route that displays book-description.handlebars based of title entered into search from homepage.handlebars

--- controllers/bookControllers.js
+++ controllers/bookControllers.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const { Book, Author, Genre } = require('../models');
 
 /////////////////////////////////////////////////////////////
@@ -13,17 +14,17 @@ router.get('/', async (req, res) => {
             include: [
                 {
                     model: Author,
-                    attributes: ['id','author_name'],
+                    attributes: ['id', 'author_name'],
                 },
             ],
         });
         // Serialize the data.
-        const books = bookData.map((book) => 
+        const books = bookData.map((book) =>
             book.get({ plain: true })
         );
 
         // Renders the data to the book template.
-        res.render('book', {books});
+        res.render('homepage', { books });
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
@@ -36,31 +37,32 @@ router.get('/:id', async (req, res) => {
             where: {
                 id: req.params.id,
             },
-
-            include: [{
-                model: Author,
-                attributes: ['id','author_name'],
-            },
-            {
-                model: Genre,
-                attributes: ['id', 'genre_name'],
-            },],
-
+            include: [
+                {
+                    model: Author,
+                    attributes: ['id', 'author_name'],
+                },
+                {
+                    model: Genre,
+                    attributes: ['id', 'genre_name'],
+                },
+            ],
         });
 
+        if (!bookData) {
+            res.status(404).json({ message: 'No book found with this id' });
+            return;
+        }
 
-        // Serialize the data.
-        const book = bookData.get({ plain: true })
-        
-        console.log(book)
-        res.render('book-description',  book );
+        const bookDetails = bookData.get({ plain: true });
+
+        res.render('book-description', { bookDetails }); // Pass the data as an object, not directly as book.
 
     } catch (err) {
         console.log(err);
-        res.status(500).json({message: 'No book found with this id'});
-    };
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 });
-
 
 
 /////////////////////////////////////////////////////////////
@@ -69,23 +71,22 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     // Create a new Book.---( Just in Case )---
-   try {
-    const newBook = await Book.create({
-        title: req.body.title,
-        book_cover: req.body.book_cover,
-        book_date: req.body.book_date,
-        author_id: req.body.author_id,
-        genre_id: req.body.genre_id,
-    });
-    console.log(newBook)
-    res.status(200).json(newBook)
-   } catch (err) {
-    res.status(400).json(err);
-   }
+    try {
+        const newBook = await Book.create({
+            title: req.body.title,
+            book_cover: req.body.book_cover,
+            book_date: req.body.book_date,
+            author_id: req.body.author_id,
+            genre_id: req.body.genre_id,
+        });
+        console.log(newBook)
+        res.status(200).json(newBook)
+    } catch (err) {
+        res.status(400).json(err);
+    }
 
 
 });
-
 
 /////////////////////////////////////////////////////////////
 /////////////// DELETE Methods for books. ///////////////////
@@ -99,57 +100,17 @@ router.delete('/:id', (req, res) => {
     })
         .then(data => {
             if (!data) {
-                res.status(404).json({message: 'No book found with the provided ID'});
+                res.status(404).json({ message: 'No book found with the provided ID' });
                 return;
             }
             res.json(data);
 
-            
+
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
 });
-
-
-//////////////////
-
-router.get('/:id', async (req, res) => {
-    try {
-      const dbBookData = await Book.findOne({
-        where: {
-          id: req.params.id,
-        },
-        include: [
-          {
-            model: Author,
-            as: 'author',
-            attributes: ['author_name'],
-          },
-        ],
-      });
-  
-      if (!dbBookData) {
-        res.status(404).json({ message: 'No book found with this id' });
-        return;
-      }
-  
-      const book = dbBookData.get({ plain: true });
-  
-      res.render('book', {
-        book,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-
-
-
-//////////////
-
-
 
 module.exports = router;
--- controllers/homeControllers.js
+++ controllers/homeControllers.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const router = require('express').Router();
 const { Author, Book, User } = require('../models');
 
@@ -21,6 +22,52 @@ router.get('/', async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
+  }
+});
+
+// Search route
+router.get('/search', async (req, res) => {
+  try {
+    const searchQuery = req.query.search;
+
+    if (!searchQuery) {
+      // If no search query, render the homepage with all books
+      const dbBookData = await Book.findAll({
+        include: [
+          {
+            model: Author,
+            attributes: ['id', 'author_name'],
+          },
+        ],
+      });
+
+      const books = dbBookData.map((book) => book.get({ plain: true }));
+
+      return res.render('homepage', { books, loggedIn: req.session.loggedIn });
+    }
+
+    // If there's a search query, find the book based on the title
+    const dbBookData = await Book.findOne({
+      where: { title: searchQuery },
+      include: [
+        {
+          model: Author,
+          attributes: ['id', 'author_name'],
+        },
+      ],
+    });
+
+    if (!dbBookData) {
+      // If the book is not found, display an error message
+      return res.render('book-not-found', { loggedIn: req.session.loggedIn });
+    }
+
+    // Render the book details page and pass the book details object to the template
+    const bookDetails = dbBookData.get({ plain: true });
+    res.render('book-description', { bookDetails, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
